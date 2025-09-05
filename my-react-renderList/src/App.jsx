772b31a281d4119239a5669b88fa6458f8e170be
Sanchali <sanchali280705@gmail.com>
2025-09-05 23:34:15 +0530

    render list

--- my-react-renderList/src/App.jsx
+++ my-react-renderList/src/App.jsx
@@ -1,6 +1,13 @@
 import List from "./List"
 function App() {
-  return(<List/>);
+  const fruits = [
+    {id:1,name:"apple" ,calories: 95},
+    {id:2,name:"orange",calories:45},
+    {id:3,name:"banana",calories:105},
+    {id:4,name:"cocunut",calories:159},
+    {id:5,name:"pinapple",calories:37}
+];
+  return(<List items={fruits} category ={"fruits"}/>);
   // print all one single string
 }
 
--- my-react-renderList/src/List.jsx
+++ my-react-renderList/src/List.jsx
@@ -1,11 +1,5 @@
-function List(){
-const fruits = [
-    {id:1,name:"apple" ,calories: 95},
-    {id:2,name:"orange",calories:45},
-    {id:3,name:"banana",calories:105},
-    {id:4,name:"cocunut",calories:159},
-    {id:5,name:"pinapple",calories:37}
-];
+function List(props){
+
 
 
 // react would like unique key for each item so it could easily keep track of items
@@ -18,22 +12,22 @@ const fruits = [
 // numerical order
 
 
-const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+// const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
     // const listItems = lowCalFruits.map(fruit => <li key={fruit.id}>
     // {fruit.name}: &nbsp;
     // <b>{fruit.calories}</b>
     // </li>);
 
 
-const highCalFruits = fruits.filter(fruit => fruit.calories >=100);
-const listItems = highCalFruits.map(fruit => <li key={fruit.id}>
-    {fruit.name}: &nbsp;
-    <b>{fruit.calories}</b>
-    </li>);
-// const listItems = fruits.map(fruit => <li key={fruit.id}>
+// const highCalFruits = fruits.filter(fruit => fruit.calories >=100);
+// const listItems = highCalFruits.map(fruit => <li key={fruit.id}>
 //     {fruit.name}: &nbsp;
 //     <b>{fruit.calories}</b>
 //     </li>);
+const listItems = fruits.map(fruit => <li key={fruit.id}>
+    {fruit.name}: &nbsp;
+    <b>{fruit.calories}</b>
+    </li>);
 
 
 return (
